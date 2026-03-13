c0844ef026bbcbb31f6ddd142fb9278d7f5d611a
Anderson Silva <Savagge999@gmail.com>
2026-03-13 10:13:20 -0300

    Exercício 02 finalizado e adicionado enunciação.

--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -1,14 +1,15 @@
-﻿namespace Exercicio01
+﻿namespace Exercicio01;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // 1.Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+        #region Enunciado
+        // 1.Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+        #endregion
 
-            Console.WriteLine("Olá, Mundo!");
+        Console.WriteLine("Olá, Mundo!");
 
-            Console.ReadKey();
-        }
+        Console.ReadKey();
     }
 }
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Enunciado
+            /* 2.Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+            Ex:
+            Qual é o seu nome? João da Silva
+            Olá João da Silva, é um prazer te conhecer!*/
+            #endregion
+
+            Console.Write("Qual é o seu nome? ");
+            var nome = Console.ReadLine().TrimStart().TrimEnd();
+            Console.WriteLine($"Olá {nome}, é um prazer te conhecer!");
+
+            Console.ReadKey();
+        }
+    }
+}
