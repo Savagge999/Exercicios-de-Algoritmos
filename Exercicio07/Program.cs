3abc8f582366ac61a2221323203d71956b26ce69
Anderson Silva <Savagge999@gmail.com>
2026-03-14 22:54:53 -0300

    Exercício 07 finalizado.

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Exercicio07;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region Enumciado
+        /* 7.Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a
+        sua terça parte.
+        Ex:
+        Digite um número: 3.5
+        O dobro de 3.5 é 7.0
+        A terça parte de 3.5 é 1.16666 */
+        #endregion
+
+        Console.Write("Digite um número: ");
+        decimal numero = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        decimal dobro = numero * 2m;
+        Console.WriteLine($"O dobro de " +
+            $"{numero.ToString(CultureInfo.InvariantCulture)} é " +
+            $"{dobro.ToString(CultureInfo.InvariantCulture)}");
+
+        decimal tercaParte = numero / 3m;
+        Console.WriteLine($"A terça parde de " +
+            $"{numero.ToString(CultureInfo.InvariantCulture)} é " +
+            $"{tercaParte.ToString("F5", CultureInfo.InvariantCulture)}");
+
+        Console.ReadKey();
+    }
+}
