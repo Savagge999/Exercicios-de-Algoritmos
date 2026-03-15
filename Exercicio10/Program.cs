cbd512895b5f831d65ca812fc7df97ed09f33cfa
Anderson Silva <Andersonsergio501@hotmail.com>
2026-03-15 11:41:11 -0300

    Exercício 10 finalizado.

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exercicio10;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region Enunciado
+        /* 10.Faça um algoritmo que leia a largura e altura de uma parede, calcule e
+        mostre a área a ser pintada e a quantidade de tinta necessária para o serviço,
+        sabendo que cada litro de tinta pinta uma área de 2metros quadrados. */
+        #endregion
+
+        Console.Write("Digite a altura em metros: ");
+        double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.Write("Digite a largura em metros: ");
+        double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double metroQuadrado = altura * largura;
+        double consumoTinta = metroQuadrado / 2.0;
+
+        Console.WriteLine($"\nÁrea total: " +
+            $"{metroQuadrado.ToString("F2", CultureInfo.InvariantCulture)} metros quadrado.");
+        Console.WriteLine($"Quantidade necessária: " +
+            $"{consumoTinta.ToString("F2", CultureInfo.InvariantCulture)} litros de tinta.");
+
+        Console.ReadKey();
+    }
+}
