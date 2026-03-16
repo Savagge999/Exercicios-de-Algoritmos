8556457be81c5105a3b02a8991baabcdda2c8f3a
Anderson Silva <Andersonsergio501@hotmail.com>
2026-03-16 13:55:07 -0300

    Exercício 11 finalizado

---
+++ Exercicio11/EquacaoSegundoGrau.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Exercicio11;
+
+internal class EquacaoSegundoGrau
+{
+
+    public double CalcularValorDelta(double a, double b, double c)
+    {
+        return Math.Pow(b, 2) - (4.0 * a * c);
+    }
+
+    public void DeltaPossuiRaizes(double a, double b, double delta)
+    {
+        if (delta < 0)
+        {
+            Console.WriteLine("A equação não possui raízes reais.");
+            return;
+        }
+
+        if (delta == 0)
+        {
+            double x = -b / (2.0 * a);
+            Console.WriteLine($"A equação apresenta uma raiz real - x: " +
+                $"{x.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        else
+        {
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine("A equação possui duas raízes reais:");
+            Console.WriteLine($"x1: {x1.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"x2: {x2.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Exercicio11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Exercicio11;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region Enunciado
+        /* 11.Desenvolva uma lógica que leia os valores de A, B e C de uma equação do
+        segundo grau e mostre o valor de Delta. */
+        #endregion
+
+        Console.Write("Digite o valor de A: ");
+        double valorA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if (valorA == 0)
+        {
+            Console.Beep();
+            Console.WriteLine("\nERRO: O valor de A não pode ser 0!");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.Write("Digite o valor de B: ");
+        double valorB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.Write("Digite o valor de C: ");
+        double valorC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        EquacaoSegundoGrau calculadora = new EquacaoSegundoGrau();
+
+        double delta = calculadora.CalcularValorDelta(valorA, valorB, valorC);
+
+        Console.WriteLine($"\nValor de Delta: " +
+            $"{delta.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        calculadora.DeltaPossuiRaizes(valorA, valorB, delta);
+
+        Console.ReadKey();
+    }
+}
