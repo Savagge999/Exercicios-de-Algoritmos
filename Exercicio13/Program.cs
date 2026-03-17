c21d55e5352a14116aa105139016edc5379ef70d
Anderson Silva <Andersonsergio501@hotmail.com>
2026-03-17 16:06:28 -0300

    Exercício 13 finalizado.

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Exercicio13;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region Enunciado
+        /* 13.Faça um algoritmo que leia o salário de um funcionário, calcule e mostre o
+        seu novo salário, com 15% de aumento. */
+        #endregion
+
+        Console.Write("Salário do funcionário: ");
+
+        if (!SalarioValido(out decimal valor))
+        {
+            Console.WriteLine($"Valor inválido!");
+            return;
+        }
+
+        decimal salarioComAumento = AumentoSalarial(valor, 15);
+
+        Console.WriteLine($"Salário com aumento de 15%: " +
+            $"{salarioComAumento.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        Console.ReadKey();
+    }
+
+    static bool SalarioValido(out decimal salario)
+    {
+        string? SalarioFuncionario = Console.ReadLine();
+
+        return decimal.TryParse(
+            SalarioFuncionario,
+            NumberStyles.Number,
+            CultureInfo.InvariantCulture,
+            out salario);
+    }
+
+    static decimal AumentoSalarial(decimal valor, decimal porcentagem)
+    {
+        return valor + (valor * porcentagem / 100);
+    }
+}
